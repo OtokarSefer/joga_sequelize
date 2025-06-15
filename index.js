df2bc0466026726ad07108e5dabda18c4f05832c
otokarsefer <otokar.sefer@voco.ee>
2025-06-15 18:53:56 +0300

    Created connection to database

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-const express = require("express")
-const app = express
+const express = require("express");
+const app = express();
 
-app.request(express.json())
-app.request(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.length("/", (req, res) => {
-    res.json({message: "welcone sequalize app"})
-})
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize("joga", "root", "Mybr41n", {
+    host: "localhost",
+    dialect: "mysql",
+});
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log("Connected to database");
+    })
+    .catch((err) => {
+        console.error("Unable to connect to the database.", err);
+    });
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to Sequelize app" });
+});
 
 app.listen(3000, () => {
-    console.log("Server http:/localhost:3000")
-})
+    console.log("Server running at http://localhost:3000");
+});
